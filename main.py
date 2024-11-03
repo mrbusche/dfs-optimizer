42077da78daf432dd56c6dc9ca0713ca76dbf44c
Matt Busche <mrbusche@gmail.com>
2024-11-03 15:42:37 -0600

    add tests (#8)
    * add tests
    
    * ignore csv again
    
    * add python path
    
    * run tests
    
    * Rename tests/draftkings.csv to draftkings.csv
    
    * make filename a param
    
    * pass in file
    
    * work around test ordering

--- main.py
+++ main.py
@@ -16,25 +16,24 @@ lineup_configs = {
     "two_te": {"QB": 1, "RB": 2, "WR": 3, "TE": 2, "DST": 1}
 }
 
-players = pd.read_csv("draftkings.csv", usecols=[PLAYER, POSITION, PROJECTION, SALARY])
-# trim whitespace from columns
-players = players.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+def calculate_lineups(lineup_type, output_file, csv_file):
+    players = pd.read_csv(csv_file, usecols=[PLAYER, POSITION, PROJECTION, SALARY])
+    # trim whitespace from columns
+    players = players.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
+    # Group players by position
+    available_players = players.groupby([POSITION, PLAYER, PROJECTION, SALARY]).size().reset_index()
 
-# Group players by position
-available_players = players.groupby([POSITION, PLAYER, PROJECTION, SALARY]).size().reset_index()
+    # Create salary and points dicts by position
+    salaries = {
+        pos: available_players[available_players[POSITION] == pos].set_index(PLAYER)[SALARY].to_dict()
+        for pos in available_players[POSITION].unique()
+    }
+    points = {
+        pos: available_players[available_players[POSITION] == pos].set_index(PLAYER)[PROJECTION].to_dict()
+        for pos in available_players[POSITION].unique()
+    }
 
-# Create salary and points dicts by position
-salaries = {
-    pos: available_players[available_players[POSITION] == pos].set_index(PLAYER)[SALARY].to_dict()
-    for pos in available_players[POSITION].unique()
-}
-points = {
-    pos: available_players[available_players[POSITION] == pos].set_index(PLAYER)[PROJECTION].to_dict()
-    for pos in available_players[POSITION].unique()
-}
-
-def calculate_lineups(lineup_type, output_file):
     lineup_results = []
     previous_lineups = []
 
@@ -86,7 +85,14 @@ def calculate_lineups(lineup_type, output_file):
 
     pd.DataFrame(lineup_results).to_csv(output_file + ".csv", index=False, header=False)
 
-for name, config in lineup_configs.items():
-    calculate_lineups(config, name)
 
-print("Lineup files created")
+def generate_lineup_files(csv_file):
+    for name, config in lineup_configs.items():
+        calculate_lineups(config, name, csv_file)
+
+    print("Lineup files created")
+
+
+if __name__ == "__main__":
+    csv_file = "draftkings.csv"  # You can change this to any CSV file name
+    generate_lineup_files(csv_file)
---
+++ tests/test_things.py
@@ -0,0 +1,100 @@
+import os
+
+import pytest
+
+from main import generate_lineup_files
+
+
+@pytest.fixture(autouse=True)
+def generate_files():
+    generate_lineup_files('./draftkings.csv')
+
+
+def test_generate_lineup_files():
+    assert os.path.exists('four_wr.csv')
+    assert os.path.exists('three_rb.csv')
+    assert os.path.exists('two_te.csv')
+
+
+def test_four_wr():
+    with open('four_wr.csv', 'r') as f:
+        four_wr = [f.readline().strip() for _ in range(10)]
+
+    # because of the duplicate scores, the results are not always returned in the same order
+    conditions = [
+        len(four_wr) == 10,
+        four_wr[0][-5:] == "145.5",
+        four_wr[1][-5:] == "145.3",
+        four_wr[2][-5:] == "145.3",
+        four_wr[3][-5:] == "145.3",
+        four_wr[4][-5:] == "145.3",
+        four_wr[5][-5:] == "145.3",
+        four_wr[6][-5:] == "145.2",
+        four_wr[7][-5:] == "145.2",
+        four_wr[8][-5:] == "145.2",
+        four_wr[9][-5:] == "145.2",
+    ]
+
+    assert all(conditions)
+
+
+def test_three_rb():
+    with open('three_rb.csv', 'r') as f:
+        three_rb = [f.readline().strip() for _ in range(10)]
+
+    conditions = [
+        len(three_rb) == 10,
+        three_rb[
+            0] == "1,DST,Cardinals,2600,7.1,QB,Lamar Jackson,8000,24.3,RB,Austin Powers,5300,16.2,RB,Chase from Paw Patrol,5900,17.9,RB,Kyren Williams,7000,21.1,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,50000,148.2",
+        three_rb[
+            1] == "2,DST,Cardinals,2600,7.1,QB,Jameis Winston,5400,17.4,RB,Alvin and the Chipmunks,7800,22.9,RB,Chase from Paw Patrol,5900,17.9,RB,Kyren Williams,7000,21.1,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49900,148.0",
+        three_rb[
+            2] == "3,DST,Cardinals,2600,7.1,QB,Jameis Winston,5400,17.4,RB,Chase from Paw Patrol,5900,17.9,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49900,148.0",
+        three_rb[
+            3] == "4,DST,Titans,3200,8.7,QB,Jameis Winston,5400,17.4,RB,Alvin and the Chipmunks,7800,22.9,RB,Austin Powers,5300,16.2,RB,Kyren Williams,7000,21.1,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49900,147.9",
+        three_rb[
+            4] == "5,DST,Titans,3200,8.7,QB,Jameis Winston,5400,17.4,RB,Austin Powers,5300,16.2,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49900,147.9",
+        three_rb[
+            5] == "6,DST,Cardinals,2600,7.1,QB,Justin Herbert,5300,17.2,RB,Alvin and the Chipmunks,7800,22.9,RB,Austin Powers,5300,16.2,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,50000,147.9",
+        three_rb[
+            6] == "7,DST,Cardinals,2600,7.1,QB,Gardner Minshew II,4500,14.7,RB,Alvin and the Chipmunks,7800,22.9,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,49900,147.8",
+        three_rb[
+            7] == "8,DST,Cardinals,2600,7.1,QB,Justin Herbert,5300,17.2,RB,Alvin and the Chipmunks,7800,22.9,RB,Chase from Paw Patrol,5900,17.9,RB,Kyren Williams,7000,21.1,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49800,147.8",
+        three_rb[
+            8] == "9,DST,Cardinals,2600,7.1,QB,Justin Herbert,5300,17.2,RB,Chase from Paw Patrol,5900,17.9,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49800,147.8",
+        three_rb[
+            9] == "10,DST,Cardinals,2600,7.1,QB,Dak Prescott,6300,19.5,RB,Alvin and the Chipmunks,7800,22.9,RB,Austin Powers,5300,16.2,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,147.7",
+    ]
+
+    assert all(conditions)
+
+
+def test_two_te():
+    with open('two_te.csv', 'r') as f:
+        two_te = [f.readline().strip() for _ in range(10)]
+
+    conditions = [
+        len(two_te) == 10,
+        two_te[
+            0] == "1,DST,Titans,3200,8.7,QB,Lamar Jackson,8000,24.3,RB,Alvin and the Chipmunks,7800,22.9,RB,Kyren Williams,7000,21.1,TE,Mike Gesicki,3100,10.6,TE,Taysom Hill,3800,9.9,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,146.0",
+        two_te[
+            1] == "2,DST,Titans,3200,8.7,QB,Lamar Jackson,8000,24.3,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,Mike Gesicki,3100,10.6,TE,Taysom Hill,3800,9.9,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,146.0",
+        two_te[
+            2] == "3,DST,Cardinals,2600,7.1,QB,Lamar Jackson,8000,24.3,RB,Alvin and the Chipmunks,7800,22.9,RB,Matt Busche,7800,22.9,TE,AJ Barner,2600,7.2,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,50000,146.0",
+        two_te[
+            3] == "4,DST,Titans,3200,8.7,QB,Justin Herbert,5300,17.2,RB,Alvin and the Chipmunks,7800,22.9,RB,Kyren Williams,7000,21.1,TE,David Njoku,5500,14.4,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,50000,145.9",
+        two_te[
+            4] == "5,DST,Titans,3200,8.7,QB,Justin Herbert,5300,17.2,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,David Njoku,5500,14.4,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,50000,145.9",
+        two_te[
+            5] == "6,DST,Titans,3200,8.7,QB,Lamar Jackson,8000,24.3,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,AJ Barner,2600,7.2,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49800,145.8",
+        two_te[
+            6] == "7,DST,Titans,3200,8.7,QB,Lamar Jackson,8000,24.3,RB,Alvin and the Chipmunks,7800,22.9,RB,Kyren Williams,7000,21.1,TE,AJ Barner,2600,7.2,TE,Mike Gesicki,3100,10.6,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,WR,Jakobi Meyers,5300,14.0,49800,145.8",
+        two_te[
+            7] == "8,DST,Titans,3200,8.7,QB,Dak Prescott,6300,19.5,RB,Alvin and the Chipmunks,7800,22.9,RB,Kyren Williams,7000,21.1,TE,David Njoku,5500,14.4,TE,Mike Gesicki,3100,10.6,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,145.7",
+        two_te[
+            8] == "9,DST,Titans,3200,8.7,QB,Dak Prescott,6300,19.5,RB,Kyren Williams,7000,21.1,RB,Matt Busche,7800,22.9,TE,David Njoku,5500,14.4,TE,Mike Gesicki,3100,10.6,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,145.7",
+        two_te[
+            9] == "10,DST,Cardinals,2600,7.1,QB,Lamar Jackson,8000,24.3,RB,Chase from Paw Patrol,5900,17.9,RB,Matt Busche,7800,22.9,TE,David Njoku,5500,14.4,TE,Mike Gesicki,3100,10.6,WR,Cedric Tillman,4300,11.5,WR,Chris Olave,6100,17.0,WR,Drake London,6700,20.0,50000,145.7",
+    ]
+
+    assert all(conditions)
