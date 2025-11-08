b9561513ccd6711be5732ab28a13535a22008632
Matt Busche <mrbusche@gmail.com>
2025-11-08 17:01:52 -0600

    clean up test cases

--- tests/test_things.py
+++ tests/test_things.py
@@ -25,16 +25,16 @@ def test_four_wr():
     # because of the duplicate scores, the results are not always returned in the same order
     conditions = [
         len(four_wr) == 10,
-        four_wr[0][-5:] == '145.5',
-        four_wr[1][-5:] == '145.3',
-        four_wr[2][-5:] == '145.3',
-        four_wr[3][-5:] == '145.3',
-        four_wr[4][-5:] == '145.3',
-        four_wr[5][-5:] == '145.3',
-        four_wr[6][-5:] == '145.2',
-        four_wr[7][-5:] == '145.2',
-        four_wr[8][-5:] == '145.2',
-        four_wr[9][-5:] == '145.2',
+        four_wr[0].endswith('145.5'),
+        four_wr[1].endswith('145.3'),
+        four_wr[2].endswith('145.3'),
+        four_wr[3].endswith('145.3'),
+        four_wr[4].endswith('145.3'),
+        four_wr[5].endswith('145.3'),
+        four_wr[6].endswith('145.2'),
+        four_wr[7].endswith('145.2'),
+        four_wr[8].endswith('145.2'),
+        four_wr[9].endswith('145.2'),
     ]
 
     assert all(conditions)
@@ -46,16 +46,16 @@ def test_three_rb():
 
     conditions = [
         len(three_rb) == 10,
-        three_rb[0][-5:] == '148.2',
-        three_rb[1][-5:] == '148.0',
-        three_rb[2][-5:] == '148.0',
-        three_rb[3][-5:] == '147.9',
-        three_rb[4][-5:] == '147.9',
-        three_rb[5][-5:] == '147.9',
-        three_rb[6][-5:] == '147.8',
-        three_rb[7][-5:] == '147.8',
-        three_rb[8][-5:] == '147.8',
-        three_rb[9][-5:] == '147.7',
+        three_rb[0].endswith('148.2'),
+        three_rb[1].endswith('148.0'),
+        three_rb[2].endswith('148.0'),
+        three_rb[3].endswith('147.9'),
+        three_rb[4].endswith('147.9'),
+        three_rb[5].endswith('147.9'),
+        three_rb[6].endswith('147.8'),
+        three_rb[7].endswith('147.8'),
+        three_rb[8].endswith('147.8'),
+        three_rb[9].endswith('147.7'),
     ]
 
     assert all(conditions)
@@ -67,16 +67,16 @@ def test_two_te():
 
     conditions = [
         len(two_te) == 10,
-        two_te[0][-5:] == '146.0',
-        two_te[1][-5:] == '146.0',
-        two_te[2][-5:] == '146.0',
-        two_te[3][-5:] == '145.9',
-        two_te[4][-5:] == '145.9',
-        two_te[5][-5:] == '145.8',
-        two_te[6][-5:] == '145.8',
-        two_te[7][-5:] == '145.7',
-        two_te[8][-5:] == '145.7',
-        two_te[9][-5:] == '145.7',
+        two_te[0].endswith('146.0'),
+        two_te[1].endswith('146.0'),
+        two_te[2].endswith('146.0'),
+        two_te[3].endswith('145.9'),
+        two_te[4].endswith('145.9'),
+        two_te[5].endswith('145.8'),
+        two_te[6].endswith('145.8'),
+        two_te[7].endswith('145.7'),
+        two_te[8].endswith('145.7'),
+        two_te[9].endswith('145.7'),
     ]
 
     assert all(conditions)
